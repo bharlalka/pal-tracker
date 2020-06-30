d8bae99fddfee6307d42a5a90f1e1746af70c58d
Bajrang Harlalka <bajrang.harlalka@dell.com>
2020-06-30 10:49:30 +0000

    Add cofnig env

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -26,6 +26,18 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var message = Configuration.GetValue<string>("WELCOME_MESSAGE");
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new ApplicationException("WELCOME_MESSAGE not configured.");
+            }
+            services.AddSingleton(sp => new WelcomeMessage(message));
+            var port = Configuration.GetValue<string>("PORT");
+            var memory = Configuration.GetValue<string>("MEMORY_LIMIT");
+            var index = Configuration.GetValue<string>("CF_INSTANCE_INDEX");
+            var address = Configuration.GetValue<string>("CF_INSTANCE_ADDR");
+            services.AddSingleton(sp => new CloudFoundryInfo(port,memory, index, address));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
